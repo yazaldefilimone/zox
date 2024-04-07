3e2be32fac60c0d216d1a0a7197aa2cd86c01b5c
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-07 23:27:46 +0200

    *

---
+++ src/utils/constants.rs
@@ -0,0 +1,1 @@
+pub const STARTING_LINE_NUMBER: i32 = 1;
---
+++ src/utils/mod.rs
@@ -0,0 +1,9 @@
+pub mod constants;
+use std::io::Read;
+
+pub fn get_read_file(path: &str) -> String {
+  let mut file = std::fs::File::open(path).unwrap();
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).unwrap();
+  contents
+}
