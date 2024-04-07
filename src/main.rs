8dd16e55e462c16526517d1b7f780daceafbca5b
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-07 23:27:46 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use rusty_jsc::JSContext;
+mod cli;
+mod core;
+mod runtime;
+mod utils;
+use core::get_context;
+use runtime::runtime::log;
+use rusty_jsc::JSValue;
+use utils::constants::STARTING_LINE_NUMBER;
+use utils::get_read_file;
 
 fn main() {
-    let mut context = JSContext::default();
-    match context.evaluate_script("'Hello World!'", 1) {
-        Ok(value) => {
-            println!("{}", value.to_string(&context).unwrap());
-        }
-        Err(e) => {
-            println!("Uncaught: {}", e.to_string(&context).unwrap())
-        }
-    }
+  let mut context = get_context();
+  let args = cli::command_line(std::env::args().collect()).unwrap();
+  let script = match args.command {
+    cli::Command::Run => get_read_file(&args.file),
+  };
+  // runtime functions
+  let global = context.get_global_object();
+  let log_callback = JSValue::callback(&context, Some(log));
+
+  let console = global.get_property(&context, "console").to_object(&context).unwrap();
+  console.set_property(&context, "log", log_callback).unwrap();
+  // ---
+  let _ = context.evaluate_script(script.as_str(), STARTING_LINE_NUMBER);
 }
