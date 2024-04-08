096113d49533d9e939204d309deba7c694d4d7ef
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-09 00:07:48 +0200

    *

--- src/runtime/runtime.rs
+++ src/runtime/runtime.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use rusty_jsc::JSValue;
 use rusty_jsc_macros::callback;
 
@@ -11,4 +12,17 @@ pub fn log(ctx: JSContext, _function: JSObject, _this: JSObject, args: &[JSValue
   println!("{}", message.join(" "));
   let result = JSValue::undefined(&ctx);
   Ok(result)
+}
+
+#[callback]
+pub fn floor(ctx: JSContext, _function: JSObject, _this: JSObject, args: &[JSValue]) -> Result<JSValue, JSValue> {
+  let value: f64 = args[0].to_number(&ctx).unwrap();
+  let result = value.floor();
+  Ok(JSValue::number(&ctx, result))
+}
+
+#[callback]
+pub fn random(ctx: JSContext, _function: JSObject, _this: JSObject, _args: &[JSValue]) -> Result<JSValue, JSValue> {
+  let result = rand::thread_rng().gen::<f64>();
+  Ok(JSValue::number(&ctx, result))
 }
