7e930341dbe971d37867dd0c0c1832eb8f0e9701
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-09 00:07:48 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod cli;
 mod core;
 mod runtime;
 mod utils;
-use core::get_context;
+use core::set_property;
+use core::{create_callback, get_context, get_property};
+use runtime::runtime::floor;
 use runtime::runtime::log;
+use runtime::runtime::random;
 use rusty_jsc::JSValue;
 use utils::constants::STARTING_LINE_NUMBER;
 use utils::get_read_file;
@@ -16,10 +19,26 @@ fn main() {
   };
   // runtime functions
   let global = context.get_global_object();
-  let log_callback = JSValue::callback(&context, Some(log));
+  let log_callback = create_callback(&context, Some(log));
+  // let floor_callback = create_callback(&context, Some(floor));
+  // let random_callback = create_callback(&context, Some(random));
 
-  let console = global.get_property(&context, "console").to_object(&context).unwrap();
-  console.set_property(&context, "log", log_callback).unwrap();
+  let console = get_property(&global, &context, "console");
+  // let math = get_property(&global, &context, "Math");
+
+  // console.set_property(&context, "log", log_callback).unwrap();
+  set_property(&console, &context, "log", log_callback).unwrap();
+  // set_property(&math, &context, "floor", floor_callback).unwrap();
+  // set_property(&math, &context, "random", random_callback).unwrap();
   // ---
+
+  // math
+  // let math = global.get_property(&context, "Math").to_object(&context).unwrap();
+  // math
+  //   .set_property(&context, "floor", JSValue::callback(&context, floor))
+  //   .unwrap();
+  // math
+  //   .set_property(&context, "random", JSValue::callback(&context, random))
+  //   .unwrap();
   let _ = context.evaluate_script(script.as_str(), STARTING_LINE_NUMBER);
 }
