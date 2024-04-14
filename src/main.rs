d23d6e4b1a98aae09ad2ab1e868f77e1370d7a2e
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-14 02:52:54 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,23 @@ mod cli;
 mod core;
 mod runtime;
 mod utils;
-use core::set_property;
-use core::{create_callback, get_context, get_property};
-use runtime::runtime::floor;
-use runtime::runtime::log;
-use runtime::runtime::random;
-use rusty_jsc::JSValue;
-use utils::constants::STARTING_LINE_NUMBER;
+use core::engine;
+use rquickjs::{CatchResultExt, Class};
+use runtime::console;
 use utils::get_read_file;
 
 fn main() {
-  let mut context = get_context();
   let args = cli::command_line(std::env::args().collect()).unwrap();
-  let script = match args.command {
+  let script_code = match args.command {
     cli::Command::Run => get_read_file(&args.file),
   };
-  // runtime functions
-  let global = context.get_global_object();
-  let log_callback = create_callback(&context, Some(log));
-  // let floor_callback = create_callback(&context, Some(floor));
-  // let random_callback = create_callback(&context, Some(random));
+  let _absolute_path = args.file;
 
-  let console = get_property(&global, &context, "console");
-  // let math = get_property(&global, &context, "Math");
-
-  // console.set_property(&context, "log", log_callback).unwrap();
-  set_property(&console, &context, "log", log_callback).unwrap();
-  // set_property(&math, &context, "floor", floor_callback).unwrap();
-  // set_property(&math, &context, "random", random_callback).unwrap();
-  // ---
-
-  // math
-  // let math = global.get_property(&context, "Math").to_object(&context).unwrap();
-  // math
-  //   .set_property(&context, "floor", JSValue::callback(&context, floor))
-  //   .unwrap();
-  // math
-  //   .set_property(&context, "random", JSValue::callback(&context, random))
-  //   .unwrap();
-  let _ = context.evaluate_script(script.as_str(), STARTING_LINE_NUMBER);
+  let (_runtime, context) = engine::create_engine();
+  let _ = context.with(|ctx| {
+    // let global_ctx = ctx.globals();
+    let console = Class::instance(ctx.clone(), console::Console {}).unwrap();
+    ctx.globals().set("console", console.clone()).unwrap();
+    ctx.eval::<(), _>(script_code).catch(&ctx).unwrap();
+  });
 }
