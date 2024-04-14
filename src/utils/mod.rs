7862c8b5633025d0c8aa2d1647a6ad16dda8934c
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-14 22:51:29 +0200

    *

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,5 @@
 pub mod constants;
+pub mod primitives;
 use std::io::Read;
 
 pub fn get_read_file(path: &str) -> String {
@@ -6,4 +7,73 @@ pub fn get_read_file(path: &str) -> String {
   let mut contents = String::new();
   file.read_to_string(&mut contents).unwrap();
   contents
+}
+
+pub fn get_transpiler_options(is_module: bool) -> String {
+  let module = if is_module {
+    r#"
+    "module": {
+        "type": "es6",
+        "strict": true,
+        "strictMode": true,
+        "lazy": false,
+        "noInterop": false,
+        "ignoreDynamic": true
+    },
+    "#
+  } else {
+    ""
+  };
+  let options = format!(
+    r#"{{
+      "minify": {{}},
+      "sourceMaps": true,
+      {}
+      "jsc": {{
+        "externalHelpers": {{}},
+        "parser": {{
+          "syntax": "typescript",
+          "jsx": true,
+          "tsx": true,
+          "decorators": true,
+          "decoratorsBeforeExport": true,
+          "dynamicImport": true,
+          "preserveAllComments": false
+        }},
+        "transform": {{
+          "legacyDecorator": true,
+          "decoratorMetadata": true,
+          "react": {{
+              "runtime": "classic",
+              "useBuiltins": true,
+              "refresh": true
+          }}
+        }},
+        "target": {{
+          "type": "es6",
+          "loose": false,
+          "spec": false,
+          "decorators": true,
+          "decoratorsBeforeExport": true,
+          "jsx": true,
+          "tsx": true,
+          "dynamicImport": true,
+          "useDefineForClassFields": true,
+          "useDefineForFunctionFields": true,
+          "useDefineForClassFields": true,
+          "useDefineForFunctionFields": true,
+          "useDefineForClassFields": true,
+          "useDefineForFunctionFields": true
+        }},
+        "keepClassNames": true
+      }}
+    }}"#,
+    module
+  );
+  options.to_string()
+}
+
+// !todo: improve this function, maybe use  low level bindings to check if the code is a module
+pub fn get_detect_module(code: &str) -> bool {
+  code.contains("import") || code.contains("export")
 }
