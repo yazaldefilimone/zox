72964929c0e7f4dc48b845613409dea268a4c2e3
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-14 02:53:54 +0200

    *

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,17 +1,2 @@
-use rusty_jsc::{JSContext, JSObject, JSObjectCallAsFunctionCallback, JSValue};
-
-pub fn get_context() -> JSContext {
-  JSContext::default()
-}
-
-pub fn get_property(object: &JSObject, ctx: &JSContext, name: &str) -> JSObject {
-  object.get_property(&ctx, "console").to_object(&ctx).unwrap()
-}
-
-pub fn create_callback(ctx: &JSContext, function: JSObjectCallAsFunctionCallback) -> JSValue {
-  JSValue::callback(ctx, function)
-}
-
-pub fn set_property(object: &JSObject, ctx: &JSContext, name: &str, value: JSValue) -> Result<(), JSValue> {
-  object.set_property(&ctx, name, value)
-}
+pub mod engine;
+pub mod event_loop;
