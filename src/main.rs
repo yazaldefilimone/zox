17a07449831f76b06a930c7f358669fef51d397e
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-15 04:06:15 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
+mod bindings;
 mod cli;
+mod compiler;
 mod core;
 mod runtime;
 mod utils;
-use core::engine;
-use rquickjs::{CatchResultExt, Class};
-use runtime::console;
+use compiler::transpiler;
+use core::event_loop;
 use utils::get_read_file;
 
 fn main() {
   let args = cli::command_line(std::env::args().collect()).unwrap();
   let script_code = match args.command {
     cli::Command::Run => get_read_file(&args.file),
+    cli::Command::Test => panic!("Test command not implemented"),
   };
   let _absolute_path = args.file;
-
-  let (_runtime, context) = engine::create_engine();
-  let _ = context.with(|ctx| {
-    // let global_ctx = ctx.globals();
-    let console = Class::instance(ctx.clone(), console::Console {}).unwrap();
-    ctx.globals().set("console", console.clone()).unwrap();
-    ctx.eval::<(), _>(script_code).catch(&ctx).unwrap();
-  });
+  let code = match args.extension {
+    cli::FileType::TypeScript => transpiler::transform(&_absolute_path, &script_code),
+    cli::FileType::JavaScript => script_code,
+    _ => panic!("Invalid file extension"),
+  };
+  event_loop::engine(&code);
 }
