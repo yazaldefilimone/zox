86b6ab24d1f00bf88f3a0b8034dfa79a92f70518
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-04-15 04:06:24 +0200

    *

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,15 @@
+#[allow(dead_code)]
 pub enum Command {
   Run,
+  Test,
 }
 
 #[allow(dead_code)]
 pub enum FileType {
   TypeScript,
+  TypeScriptReact,
   JavaScript,
+  Vue,
 }
 
 pub struct CliResult {
@@ -14,28 +18,34 @@ pub struct CliResult {
   pub file: String,
 }
 pub fn command_line(args: Vec<String>) -> Result<CliResult, String> {
-  let command = match args.get(1) {
-    Some(command) => command.to_string(),
-    None => "run".to_string(),
-  };
-  // let extension = match args.get(2).and_then(|s| s.rfind('.')) {
-  //   Some(extension) => match extension.to_string().as_str() {
-  //     "ts" => FileType::TypeScript,
-  //     "js" => FileType::JavaScript,
-  //     _ => {
-  //       return Err(format!("Unknown file extension: {}", extension.to_string().as_str()));
-  //     }
-  //   },
-  //   None => {
-  //     return Err(format!("No file extension"));
-  //   }
-  // };
-  let command = match command.as_str() {
-    "run" => Command::Run,
-    _ => {
-      return Err(format!("Unknown command: {}", command));
-    }
-  };
+  let start_commands: usize = 1;
+  // todo: verify if is development or production
+  // let environment = env::var("CARGO");
 
-  Ok(CliResult { extension: FileType::JavaScript, command, file: args.get(2).unwrap().to_string() })
+  // zox [command] [file]
+  match args.len() {
+    2 => {
+      let file = &args[start_commands];
+      let extension = match file.split('.').last() {
+        Some("ts") => FileType::TypeScript,
+        Some("js") => FileType::JavaScript,
+        _ => return Err("Invalid file extension".to_string()),
+      };
+      Ok(CliResult { extension, command: Command::Run, file: file.to_string() })
+    }
+    3 => {
+      let command = match args[start_commands].as_str() {
+        "run" => Command::Run,
+        _ => return Err("Invalid command".to_string()),
+      };
+      let file = &args[start_commands + 1];
+      let extension = match file.split('.').last() {
+        Some("ts") => FileType::TypeScript,
+        Some("js") => FileType::JavaScript,
+        _ => return Err("Invalid file extension".to_string()),
+      };
+      Ok(CliResult { extension, command, file: file.to_string() })
+    }
+    _ => Err("Too many arguments provided".to_string()),
+  }
 }
